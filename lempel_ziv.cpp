626ae328a931d3f7ff32cb3075933c3bab5a726c
jgodoy2022@ <jgodoy2022@udec.cl>
2024-06-15 01:26:12 -0400

    feat: Funciones de creacion de archivos txt y calculo de peso del archivo en lempel_ziv

--- lempel_ziv.cpp
+++ lempel_ziv.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <iterator>
 #include <chrono>
+#include <sstream>  // Incluir la cabecera para std::stringstream
 
 struct TrieNode {
     std::unordered_map<char, TrieNode*> children;
@@ -90,7 +91,27 @@ public:
 
         return decompressed;
     }
+    double obtener_peso_archivo(const std::string& filename) {
+        std::ifstream file(filename, std::ios::binary | std::ios::ate);
+        if (!file.is_open()) {
+            std::cerr << "No se puede abrir el archivo: " << filename << std::endl;
+            return 0.0;
+        }
+        std::streamsize size = file.tellg();
+        file.close();
+        return size / 1000.0; // tamaño en kilobytes
+    }
+    void escribir_a_archivo(const std::string& filename, const std::string& texto) {
+        std::ofstream file(filename);
+        if (file.is_open()) {
+            file << texto;
+            file.close();
+        } else {
+            std::cerr << "No se puede abrir el archivo para escribir: " << filename << std::endl;
+        }
+    }
 };
+
 
 int main() {
     std::ifstream archivo("test.txt");
@@ -105,25 +126,47 @@ int main() {
 
     Trie trie;
 
+    std::cout << "Texto original: " << texto << std::endl;
     auto inicio_com = std::chrono::high_resolution_clock::now();
     auto compressed = trie.compress(texto);
     auto fin_com = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> duracion_com = fin_com - inicio_com;
 
-    std::cout << "Compressed output: \n";
+    std::stringstream texto_comprimido_ss;
+    std::string texto_comprimido;
+
+    std::cout << "Compressed output: ";
     for (const auto& p : compressed) {
         std::cout << "(" << p.first << "," << p.second << ") ";
+        texto_comprimido_ss << "(" << p.first << "," << p.second << ") ";
     }
     std::cout << std::endl;
+
+    texto_comprimido = texto_comprimido_ss.str();
 
     auto inicio_decom = std::chrono::high_resolution_clock::now();
     std::string decompressed = trie.decompress(compressed);
     auto fin_decom = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> duracion_decom = fin_decom - inicio_decom;
 
-    std::cout << "Decompressed output: " << decompressed << std::endl;
+    std::cout << "Decompressed output: " << decompressed << std::endl << std::endl;
 
-     std::cout << "Tiempo de compresion: " << duracion_com.count() << std::endl;
+    trie.escribir_a_archivo("texto_comprimido.txt", texto_comprimido);
+    trie.escribir_a_archivo("texto_descomprimido.txt", decompressed);
+
+    double peso_original = trie.obtener_peso_archivo("test.txt");
+    double peso_comprimido = trie.obtener_peso_archivo("texto_comprimido.txt");
+    double peso_descomprimido = trie.obtener_peso_archivo("texto_descomprimido.txt");
+
+    //Imprime los pesos de los textos
+    std::cout << "Tamanos: " << std::endl;
+    std::cout << "Peso del texto original (en kilobytes): " << peso_original << " KB" << std::endl;
+    std::cout << "Peso del texto comprimido (en kilobytes): " << peso_comprimido << " KB" << std::endl;
+    std::cout << "Peso del texto descomprimido (en kilobytes): " << peso_descomprimido << " KB" << std::endl << std::endl;
+
+
+    std::cout << "Tiempo: " <<  std::endl;
+    std::cout << "Tiempo de compresion: " << duracion_com.count() << std::endl;
     std::cout << "Tiempo de decompresion: " << duracion_decom.count() << std::endl;
 
     return 0;
