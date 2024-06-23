9c167816fbafb8ec54c080c7218b638f10903f11
bvillarroelr <benjamin.villarroel3@gmail.com>
2024-06-22 23:34:17 -0400

    fix: corrección de error en el script de huffman

--- huffman_code/experiment_huffman.sh
+++ huffman_code/experiment_huffman.sh
@@ -15,10 +15,10 @@ num_ejecuciones=20
 # Crear o vaciar el archivo CSV y escribir la cabecera
 echo "Archivo,Tiempo_Codificacion,Tiempo_Decodificacion,Peso_Original,Peso_Codificado" > $archivo_csv
 
-# Lista de archivos en la carpeta de entrada
+# Lista de archivos en la carpeta de entrada (descomentar lo que se desee ejecutar)
 archivos=("dna1MB.txt" "dna5MB.txt" "dna10MB.txt" "dna20MB.txt" "dna50MB.txt")
-archivos=("dna1MB.txt" "dna5MB.txt" "dna10MB.txt" "dna20MB.txt" "dna50MB.txt")
-archivos=("dna1MB.txt" "dna5MB.txt" "dna10MB.txt" "dna20MB.txt" "dna50MB.txt")
+#archivos=("proteins1MB.txt" "proteins5MB.txt" "proteins10MB.txt" "proteins20MB.txt" "proteins50MB.txt")
+#archivos=("english1MB.txt" "english5MB.txt" "english10MB.txt" "english20MB.txt" "english50MB.txt")
 
 # Ejecutar el programa para cada archivo y capturar los tiempos de ejecución
 for archivo in "${archivos[@]}"
